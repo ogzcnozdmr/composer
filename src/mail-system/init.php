f42999a4cc7fb488281a012151750c5bd6c4a75f
Oğuzcan ÖZDEMİR <ce.oguzcan.ozdemir@gmail.com>
2021-02-14 22:38:33 +0300

    hello

---
+++ src/mail-system/init.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Oguzcan;
+
+class init {
+
+    public function __construct()
+    {
+        //return "hello";
+    }
+
+    /**
+     * @return string
+     */
+    public function try(){
+        return "hello";
+    }
+
+}
